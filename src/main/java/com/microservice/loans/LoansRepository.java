c0fa2827f63b52ab7fbd77c3b5fcf4409af3025c
Pedro Antônio <pedroaia02@gmail.com>
2024-09-11 11:54:11 -0300

    feat: Loans repository

---
+++ src/main/java/com/microservice/loans/LoansRepository.java
@@ -0,0 +1,11 @@
+package com.microservice.loans;
+
+
+import com.microservice.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+}
